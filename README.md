80cb4c85ac4ba5dc7ead11dba7afbe5a0aa8cab4
Phil Elson <philip.elson@cern.ch>
2024-08-05 14:37:26 +0200

    Implement a basic build command which works for wheel creation (doesn't work for sdist or editable)

--- README.md
+++ README.md
@@ -7,4 +7,33 @@ The idea is to be able to maintain the modern typing in your
 repository, and then as part of the build stage, convert the
 code to the older form.
 
+## Build-time transformation
 
+`typing-to-the-future` includes a custom `setuptools` build_py command to
+transform Python files into the compatibility form when creating a wheel
+using any PEP-517 build backend. Currently, both editable mode and sdist
+conversions are not supported (for sdists, this isn't such a concern, since
+the conversion will take place when the sdist is being installed as a wheel).
+
+To setup a build-time conversion, add the following to `setup.py` (it can be
+the only content of `setup.py` if `pyproject.toml` is used for metadata):
+
+```
+from typing_to_the_future.build_cmd import cmd_class
+from setuptools import setup
+
+setup(
+   cmdclass=cmd_class(),
+)
+```
+
+## Available transformations
+
+For all transformations, an `import typing` will be injected where necessary
+and appropriate.
+
+* `A | B` -> `typing.Union[A, B]`
+
+## Transformations not yet implemented
+
+* `A | None` -> `typing.Optional[A]`
---
+++ typing_to_the_future/build_cmd.py
@@ -0,0 +1,51 @@
+import typing
+
+try:
+    import setuptools
+except ImportError:
+    raise ImportError("Unable to import setuptools. Be sure to declare it as a build dependency")
+
+
+# Follow the advice in https://github.com/pypa/setuptools/discussions/3762.
+
+import logging
+from pathlib import Path
+from setuptools.command.build_py import build_py
+
+import libcst as cst
+
+from ._converters import convert_union
+
+
+class ConvertCodeToLegacyForm(build_py):
+    def convert_to_legacy(self, py_path: str) -> bool:
+        """
+        Convert the given code to legacy form. Return True if changes made.
+
+        """
+        mod = cst.parse_module(Path(py_path).read_text())
+        new = convert_union(mod)
+        if mod.code != new.code:
+            Path(py_path).write_text(new.code)
+
+    def build_module(self, module, module_file, package):
+        outfile, copied = super().build_module(module, module_file, package)
+
+        # self.has_announced_downgrade = getattr(self, 'has_announced_downgrade', False)
+        # if not self.has_announced_downgrade:
+        #     self.announce(f'Downgrading modern code to legacy form', level=logging.INFO)
+        #     self.has_announced_downgrade = True
+
+        if self.convert_to_legacy(outfile) and copied:
+            self.announce(f'Downgrading {outfile}', level=logging.INFO)
+            # Access the name-mangled dict... :(
+            self._build_py__updated_files.append(outfile)
+        return outfile, copied
+
+
+def cmd_class(
+        existing_cmd_class: typing.Optional[typing.Dict[str, typing.Type]] = None,
+) -> typing.Dict[str, typing.Type]:
+    return dict(**(existing_cmd_class.items()) or {}, **{
+        'build_py': ConvertCodeToLegacyForm,
+    })
