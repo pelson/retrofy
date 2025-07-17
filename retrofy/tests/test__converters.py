3fe4c49f8ce1d0484259331af06532fb3416df7a
Phil Elson <philip.elson@cern.ch>
2025-07-17 11:11:24 +0200

    Fix the case where we have from __future__ import annotations

--- retrofy/tests/test__converters.py
+++ retrofy/tests/test__converters.py
@@ -27,6 +27,23 @@ def test_union():
     assert result.code == expected
 
 
+def test_union_with_docstring():
+    test_case_source = textwrap.dedent("""
+    '''Some module'''
+
+    c: unknown | int
+    """)
+    expected = textwrap.dedent("""
+    '''Some module'''
+    import typing
+
+    c: typing.Union[unknown, int]
+    """)
+    module = cst.parse_module(test_case_source)
+    result = _converters.convert_union(module)
+    assert result.code == expected
+
+
 def test_union__future__():
     test_case_source = textwrap.dedent("""
     from __future__ import annotations
@@ -35,10 +52,58 @@ def test_union__future__():
     """)
 
     expected = textwrap.dedent("""
-    import typing
     from __future__ import annotations
+    import typing
 
     c: typing.Union[unknown, int]
+    """)
+    module = cst.parse_module(test_case_source)
+    result = _converters.convert_union(module)
+    assert result.code == expected
+
+
+def test_union__future___with_docstring():
+    test_case_source = textwrap.dedent("""
+    '''
+    Some module
+    '''
+
+    from __future__ import annotations
+
+    c: unknown | int
+    """)
+
+    expected = textwrap.dedent("""
+    '''
+    Some module
+    '''
+
+    from __future__ import annotations
+    import typing
+
+    c: typing.Union[unknown, int]
+    """)
+    module = cst.parse_module(test_case_source)
+    result = _converters.convert_union(module)
+    assert result.code == expected
+
+
+def test_union__future__with_import_already():
+    test_case_source = textwrap.dedent("""
+    from __future__ import annotations
+    import typing
+
+    if typing.TYPE_CHECKING:
+        c: str | int
+    """)
+
+    expected = textwrap.dedent("""
+    from __future__ import annotations
+    import typing
+    import typing
+
+    if typing.TYPE_CHECKING:
+        c: typing.Union[str, int]
     """)
     module = cst.parse_module(test_case_source)
     result = _converters.convert_union(module)
